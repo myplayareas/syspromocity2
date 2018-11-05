00b1fc62524db6d7875eaf58032ffc6982256990
armandossrecife <armando.sousa@gmail.com>
2018-11-05 20:18:29 -0300

    Concluído o CRUD básico para manipular lojas, promoções e cupons.

--- src/main/java/br/ufc/great/syspromocity/controller/StoreController.java
+++ src/main/java/br/ufc/great/syspromocity/controller/StoreController.java
@@ -1,5 +1,8 @@
 package br.ufc.great.syspromocity.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.ufc.great.syspromocity.model.Coupon;
+import br.ufc.great.syspromocity.model.Promotion;
 import br.ufc.great.syspromocity.model.Store;
 import br.ufc.great.syspromocity.service.StoresService;
 
@@ -16,7 +21,9 @@ import br.ufc.great.syspromocity.service.StoresService;
 public class StoreController {
 
     private StoresService storeService;
-
+    private List<Promotion> listPromotions=null;
+	private List<Coupon> listCoupons;
+    
     @Autowired
     public void setCouponService(StoresService storeService) {
         this.storeService = storeService;
@@ -43,7 +50,7 @@ public class StoreController {
         return "stores/list";
 
     }
-    //TODO inserir uma lista de promoções em uma tabela de promoções com as ações (inserir, editar, remover)
+    
     @RequestMapping("/stores/add")
     public String add(Model model) {
 
@@ -52,7 +59,6 @@ public class StoreController {
 
     }
 
-  //TODO para a loja selecionada listar as promoções associados com uma tabela de promoões com as ações (inserir, editar, remover)
     @RequestMapping("/stores/edit/{id}")
     public String edit(@PathVariable Long id, Model model) {
 
@@ -61,7 +67,6 @@ public class StoreController {
 
     }
 
-   //TODO para a loja selecionada listar as promoções associados com uma tabela de promoções com as ações (inserir, editar, remover)
     @RequestMapping(value = "/stores/save", method = RequestMethod.POST)
     public String save(Store store, final RedirectAttributes ra) {
 
@@ -70,13 +75,130 @@ public class StoreController {
         return "redirect:/stores";
 
     }
+    
+    //save new Promotion
+    @RequestMapping(value = "/stores/{id}/promotions/save", method = RequestMethod.POST)
+    public String save(@PathVariable("id") Integer id,Promotion promotion, final RedirectAttributes ra) {
+    	
+    	Store store = storeService.get(Long.valueOf(id));
+    	store.getPromotionList().add(promotion);
+    	
+        Store save = storeService.save(store);
+        
+        ra.addFlashAttribute("successFlash", "Loja foi salva com nova promoção.");
+        return "redirect:/stores";
+    }
+
 
     @RequestMapping("/stores/delete/{id}")
     public String delete(@PathVariable Long id) {
 
         storeService.delete(id);
         return "redirect:/stores";
+    }
+    
+    @RequestMapping(value="/stores/{id}/promotions", method=RequestMethod.GET)
+    public String listPromotions(@PathVariable("id") Long id, Model model) {
+    	List<Promotion> listPromotionsAux = storeService.get(Long.valueOf(id)).getPromotionList();
+    	this.listPromotions = listPromotionsAux;
+    	
+    	model.addAttribute("idStore", id);
+        model.addAttribute("list", listPromotionsAux);
+        
+    	return "stores/listPromotions";
+    }
+
+    //add Promotion in Store
+    @RequestMapping("/stores/{id}/promotions/add")
+    public String addPromotion(@PathVariable("id") Integer id, Model model) {
+    	
+    	List<Coupon> emptyList = new LinkedList<Coupon>();
+    	this.listCoupons = emptyList; 
+    	
+    	model.addAttribute("idStore", id);
+        model.addAttribute("promotion", new Promotion());
+        return "stores/formPromotions";
+
+    }
+       
+    //Editar uma promoção de uma loja stores/edit/idStore/promotions/edit/idPromotion
+    /*
+     * Dada uma loja, 
+     * recuperar a lista de promoções
+     * Dada a lista de promoções, localizar uma promoção
+     */
+    @RequestMapping("/stores/edit/{idStore}/promotions/edit/{idPromotion}")
+    public String editPromotion(@PathVariable Long idStore, @PathVariable Long idPromotion, Model model) {    	
+    	List<Promotion> listPromotionsAux = storeService.get(idStore).getPromotionList();
+    	Promotion promotion = new Promotion();
+    	
+    	this.listPromotions = listPromotionsAux;
+    	
+    	//Procura a promoção, na lista de promoções da loja, que vai ser editada
+    	for (Promotion element : listPromotionsAux) {
+    		if (element.getId()==idPromotion) {
+    			promotion.setId(element.getId());
+    			promotion.setDescription(element.getDescription());
+    			promotion.setFromDate(element.getFromDate());
+    			promotion.setToDate(element.getToDate());
+    			promotion.setCoupons(this.listCoupons);
+    		}
+    	}
+    	
+        model.addAttribute("store", storeService.get(idStore));  
+        model.addAttribute("idStore", idStore);
+        model.addAttribute("list", listPromotionsAux);
+        model.addAttribute("promotion", promotion);
+        model.addAttribute("idPromotion", idPromotion);
+
+        return "stores/formEditPromotion";
 
     }
 
+    //save Promotion in Store
+    @RequestMapping(value = "/stores/{idStore}/promotions/save/{idPromotion}", method = RequestMethod.POST)
+    public String savePromotion(@PathVariable("idStore") Integer idStore,@PathVariable("idPromotion") Integer idPromotion,Promotion promotion, final RedirectAttributes ra) {
+    	Store store = storeService.get(Long.valueOf(idStore));
+    	
+    	//Procura a promoção, com os dados antigos, na lista de promoção da loja
+    	for (Promotion element : store.getPromotionList()) {
+    		if (element.getId()==Long.valueOf(idPromotion)) {
+    			//remove a promoção antiga da lista de promoções
+    			store.getPromotionList().remove(element);
+    			break;
+    		}    		
+    	}
+    	
+    	store.getPromotionList().add(promotion);
+    	
+    	//salva a loja com os novos dados de promoão
+        Store save = storeService.save(store);
+        
+        ra.addFlashAttribute("successFlash", "Os novos dados da promoção foi salva na Loja.");
+        return "redirect:/stores";
+    }
+    
+    //Remover uma promoção de uma dada loja stores/edit/idStore/promotions/delete/idPromotion
+    @RequestMapping(value = "/stores/edit/{idStore}/promotions/delete/{idPromotion}")
+    public String deletePromotion(@PathVariable("idStore") Integer idStore, @PathVariable("idPromotion") Integer idPromotion, final RedirectAttributes ra) {
+  
+    	Store store = storeService.get(Long.valueOf(idStore));
+    	
+    	//Procura a promoção na lista de promoções da loja
+    	for (Promotion element : store.getPromotionList()) {
+    		if (element.getId()==Long.valueOf(idPromotion)) {
+    			store.getPromotionList().remove(element);
+    			break;
+    		}    		
+    	}
+    	
+    	//salva a promoção depois do cupom removido
+        Store save = storeService.save(store);
+        
+        ra.addFlashAttribute("successFlash", "A promoção foi removida da lista.");
+        return "redirect:/stores";
+    }
+
+
+    
 }
